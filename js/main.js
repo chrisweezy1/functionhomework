90f5ff7e71b88593a12d0f2f288bff85a610cce2
chris <cleon5886@gmail.com>
2019-03-24 23:55:53 +0100

    homework

---
+++ js/main.js
@@ -0,0 +1,20 @@
+class Main {
+	execute(){
+		//this is one comment
+		//your code 
+
+		
+		let water = [ "beach" , "river" , "lake", "pool" , "swam" , "sea"];
+
+		
+		function waterbodies(num) {
+			return water[num];
+		}     
+
+		waterbodies(4);
+
+	} 
+
+}
+
+new Main().execute();
